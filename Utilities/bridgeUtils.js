44f688495a267cb659d717046b430c1fe48da980
Erriccc <97201349+Erriccc@users.noreply.github.com>
2022-10-09 04:35:15 -0500

    crosschain functionality

--- Utilities/bridgeUtils.js
+++ Utilities/bridgeUtils.js
@@ -8,4 +8,3 @@ const config = JSON.parse(fs.readFileSync('./xdapp.config.json').toString());
 
 
 const OriginchainContract =  "i"
-const pvk = '87c24a6566f51e0f2b52c9ebf1bdc449206b4b3e8bc6eb593e8da72d895b3c54';
