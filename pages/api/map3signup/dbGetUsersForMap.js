4ea3904c7588846d6d91505633463465e013774d
notfinancialadvice <notfinancialadvice@notfinancialadvices-MacBook-Pro.local>
2023-04-13 14:26:53 -0500

    deleteing unused files2

--- pages/api/map3signup/dbGetUsersForMap.js
+++ pages/api/map3signup/dbGetUsersForMap.js
@@ -21,26 +21,27 @@ export default async function form(req, res) {
    
     else{ // PUT' REQUEST
 
+      console.log('// PUT REQUEST // PUT REQUESThefjbbfefeb;ffbfebedbedvdvdjlvdev')
+
       if (
         !body.string ){
         
         // || body.string.legnth < 1 || body.string == '') {
       return res.status(400).json({ data: 'incomplete required parameters' })
     }
-      if(body.string.toLowerCase() == '*' || body.string.legnth < 1 || body.string == ''){
+      if(body.string === '*' || body.string.legnth < 1 || body.string === ''){
         console.log('every data was requested')
         // map3Vendors = vendorsData;
         // getAll
-        console.log('// PUT REQUEST // PUT REQUESThefjbbfefeb;ffbfebedbedvdvdjlvdev')
         const users = await getAll()
           res.status(200).json(users)
-  
-    
-  
+      }else{
+
+        console.log('string was requested')
+        const users = await searchForManyByKeywordsOrBioOrName(body.string)
+          res.status(200).json(users)
       }
-      console.log('// PUT REQUEST // PUT REQUESThefjbbfefeb;ffbfebedbedvdvdjlvdev')
-      const users = await searchForManyByKeywordsOrBioOrName(body.string)
-        res.status(200).json(users)
+      
     }
   
 
